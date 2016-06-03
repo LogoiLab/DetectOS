13827bec1e174c2ab4c8cd24ef3100b86f741eb4
LogoiLab <LogoiLab@GitHub.com>
2016-06-03 12:44:07 -0400

    realized some mistakes

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,15 @@
 mod mac;
 mod win;
 mod nix;
-pub mod detect{
-    fn main(arch: arch, version: version, vendor: vendor) {
-        struct os {
-            arch: arch,
-            version: version,
-            vendor: vendor
-        }
-        impl os {
-            fn new() -> os {
+pub mod detect {
+    mod arch {
 
-            }
-        }
-        let mut os = os.new();
-        os.add_arch(arch);
-        os.add_version(version);
-        os.add_vendor(vendor);
     }
+    mod version {
+
+    }
+    mod vendor {
+
+    }
+    
 }
